e545eff04b04841b6dd84b9a0e6fe9032087f803
Markus Korbel <markus.korbel@gmail.com>
2021-05-02 15:32:55 +0100

    Import of asp.net core api project template
    Tested on ubuntu server 20.04 vm

---
+++ OpenSky.API/Controllers/AirportController.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WeatherForecastController.cs" company="OpenSky">
+// sushi.at for OpenSky 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    using OpenSky.API.Model;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Airport controller.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 02/05/2021.
+    /// </remarks>
+    /// <seealso cref="T:Microsoft.AspNetCore.Mvc.ControllerBase"/>
+    /// -------------------------------------------------------------------------------------------------
+    [ApiController]
+    [Route("[controller]")]
+    public class AirportController : ControllerBase
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        private readonly ILogger<AirportController> logger;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AirportController"/> class.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 02/05/2021.
+        /// </remarks>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        public AirportController(ILogger<AirportController> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the ICAO identifiers of all available airports.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 02/05/2021.
+        /// </remarks>
+        /// <returns>
+        /// The ICAO identifiers of all available airports.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        [HttpGet]
+        public IEnumerable<Airport> Get()
+        {
+            return new Airport[] { new Airport { ICAO = "LOWW" }, new Airport { ICAO = "LOWG" } };
+        }
+    }
+}
---
+++ OpenSky.API/Startup.cs
@@ -0,0 +1,95 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Startup.cs" company="OpenSky">
+// sushi.at for OpenSky 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.OpenApi.Models;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// API startup class.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 02/05/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public class Startup
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 02/05/2021.
+        /// </remarks>
+        /// <param name="configuration">
+        /// Gets the configuration.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the configuration.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        public IConfiguration Configuration { get; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request
+        /// pipeline.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 02/05/2021.
+        /// </remarks>
+        /// <param name="app">
+        /// The application.
+        /// </param>
+        /// <param name="env">
+        /// The environment.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OpenSky.API v1"));
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 02/05/2021.
+        /// </remarks>
+        /// <param name="services">
+        /// The services collection.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "OpenSky.API", Version = "v1" }); });
+        }
+    }
+}
