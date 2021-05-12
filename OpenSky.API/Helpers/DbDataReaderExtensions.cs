b281f1ee80a33f1bd1a72add1c8c8e0506033d10
Markus Korbel <markus.korbel@gmail.com>
2021-05-12 17:46:05 +0100

    Got full littlenavmap database import worker completed
    Adds runways(+ends), and approaches
    Processed on background worker
    Tried to get performance as fast possible (tested multiple approaches)
    Currently full import from dev container takes >17 minutes, will test on release

---
+++ OpenSky.API/Helpers/DbDataReaderExtensions.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DbDataReaderExtensions.cs" company="OpenSky">
+// sushi.at for OpenSky 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API.Helpers
+{
+    using System.Data.Common;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Database reader extension methods.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 12/05/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public static class DbDataReaderExtensions
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// A DbDataReader extension method that calculates the SHA-1 hash code by combining all columns to a single string.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 12/05/2021.
+        /// </remarks>
+        /// <param name="reader">
+        /// The reader to act on.
+        /// </param>
+        /// <returns>
+        /// The calculated hash code (hex string).
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        public static string CalculateHashCode(this DbDataReader reader)
+        {
+            var stringValue = string.Empty;
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                if (!reader.IsDBNull(i))
+                {
+                    stringValue += reader.GetValue(i).ToString();
+                }
+                else
+                {
+                    stringValue += "NULL|";
+                }
+            }
+
+            var hash = new SHA1Managed().ComputeHash(Encoding.UTF8.GetBytes(stringValue));
+            return string.Concat(hash.Select(b => b.ToString("x2")));
+        }
+    }
+}
--- OpenSky.API/Migrations/20210512154307_RunwayAndApproachWithDataImport.cs
+++ OpenSky.API/Migrations/20210512154307_RunwayAndApproachWithDataImport.cs
@@ -1,11 +1,39 @@
 ﻿using System;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace OpenSky.API.Migrations
 {
-    public partial class RunwayAndApproachesWithDataImport : Migration
+    /// -------------------------------------------------------------------------------------------------
+    /// <content>
+    /// Runway and approach with support for data imports migration.
+    /// </content>
+    /// -------------------------------------------------------------------------------------------------
+    public partial class RunwayAndApproachWithDataImport : Migration
     {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// <para>
+        ///                 Builds the operations that will migrate the database 'up'.
+        ///             </para>
+        /// <para>
+        ///                 That is, builds the operations that will take the database from the state
+        ///                 left in by the previous migration so that it is up-to-date with regard to
+        ///                 this migration.
+        ///             </para>
+        /// <para>
+        ///                 This method must be overridden in each class the inherits from
+        ///                 <see cref="T:Microsoft.EntityFrameworkCore.Migrations.Migration" />.
+        ///             </para>
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 12/05/2021.
+        /// </remarks>
+        /// <param name="migrationBuilder">
+        /// The <see cref="T:Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder" /> that will
+        /// build the operations.
+        /// </param>
+        /// <seealso cref="M:Microsoft.EntityFrameworkCore.Migrations.Migration.Up(MigrationBuilder)"/>
+        /// -------------------------------------------------------------------------------------------------
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.RenameColumn(
@@ -13,13 +41,21 @@ namespace OpenSky.API.Migrations
                 table: "Airports",
                 newName: "RunwayCount");
 
+            migrationBuilder.AddColumn<string>(
+                name: "HashCode",
+                table: "Airports",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
             migrationBuilder.CreateTable(
                 name: "Approaches",
                 columns: table => new
                 {
-                    ID = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ID = table.Column<int>(type: "int", nullable: false),
                     AirportICAO = table.Column<string>(type: "varchar(5)", maxLength: 5, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    HashCode = table.Column<string>(type: "longtext", nullable: false)
                         .Annotation("MySql:CharSet", "utf8mb4"),
                     RunwayName = table.Column<string>(type: "varchar(6)", maxLength: 6, nullable: true)
                         .Annotation("MySql:CharSet", "utf8mb4"),
@@ -67,14 +103,15 @@ namespace OpenSky.API.Migrations
                 name: "Runways",
                 columns: table => new
                 {
-                    ID = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ID = table.Column<int>(type: "int", nullable: false),
                     AirportICAO = table.Column<string>(type: "varchar(5)", maxLength: 5, nullable: false)
                         .Annotation("MySql:CharSet", "utf8mb4"),
                     Altitude = table.Column<int>(type: "int", nullable: false),
                     CenterLight = table.Column<string>(type: "varchar(1)", maxLength: 1, nullable: true)
                         .Annotation("MySql:CharSet", "utf8mb4"),
                     EdgeLight = table.Column<string>(type: "varchar(1)", maxLength: 1, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    HashCode = table.Column<string>(type: "longtext", nullable: false)
                         .Annotation("MySql:CharSet", "utf8mb4"),
                     Length = table.Column<int>(type: "int", nullable: false),
                     Surface = table.Column<string>(type: "varchar(7)", maxLength: 7, nullable: false)
@@ -97,11 +134,12 @@ namespace OpenSky.API.Migrations
                 name: "RunwayEnds",
                 columns: table => new
                 {
-                    ID = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ID = table.Column<int>(type: "int", nullable: false),
                     ApproachLightSystem = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: true)
                         .Annotation("MySql:CharSet", "utf8mb4"),
                     HasClosedMarkings = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    HashCode = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
                     Heading = table.Column<double>(type: "double", nullable: false),
                     Latitude = table.Column<double>(type: "double", nullable: false),
                     LeftVasiPitch = table.Column<double>(type: "double", nullable: true),
@@ -144,6 +182,33 @@ namespace OpenSky.API.Migrations
                 column: "AirportICAO");
         }
 
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// <para>
+        ///                 Builds the operations that will migrate the database 'down'.
+        ///             </para>
+        /// <para>
+        ///                 That is, builds the operations that will take the database from the state
+        ///                 left in by this migration so that it returns to the state that it was in
+        ///                 before this migration was applied.
+        ///             </para>
+        /// <para>
+        ///                 This method must be overridden in each class the inherits from
+        ///                 <see cref="T:Microsoft.EntityFrameworkCore.Migrations.Migration" /> if both
+        ///                 'up' and 'down' migrations are to be supported. If it is not overridden, then
+        ///                 calling it will throw and it will not be possible to migrate in the 'down'
+        ///                 direction.
+        ///             </para>
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 12/05/2021.
+        /// </remarks>
+        /// <param name="migrationBuilder">
+        /// The <see cref="T:Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder" /> that will
+        /// build the operations.
+        /// </param>
+        /// <seealso cref="M:Microsoft.EntityFrameworkCore.Migrations.Migration.Down(MigrationBuilder)"/>
+        /// -------------------------------------------------------------------------------------------------
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
@@ -157,6 +222,10 @@ namespace OpenSky.API.Migrations
 
             migrationBuilder.DropTable(
                 name: "Runways");
+
+            migrationBuilder.DropColumn(
+                name: "HashCode",
+                table: "Airports");
 
             migrationBuilder.RenameColumn(
                 name: "RunwayCount",
