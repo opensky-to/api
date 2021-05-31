cc22e9e2fff1a0f8c9f56613c4464721924478ae
sushi.at <sushi@opensky.to>
2021-06-01 00:56:55 +0100

    Added application token
    Renamed logout to revokeToken
    Added revoke all tokens method
    Added reset tokens? parameter to password change methods

---
+++ OpenSky.API/Model/Authentication/ApplicationToken.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ApplicationToken.cs" company="OpenSky">
+// sushi.at for OpenSky 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API.Model.Authentication
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Application token model.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 31/05/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public class ApplicationToken
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets or sets the name of the application.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        [Required(ErrorMessage = "Application name is required")]
+        public string Name { get; set; }
+    }
+}
