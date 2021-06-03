12cf4c25f9f6e74adb60a9e0131d45ce7487a8a2
Markus Korbel <markus.korbel@gmail.com>
2021-06-03 10:52:24 +0100

    Documented and cleaned up enum documentation filter

--- OpenSky.API/SwaggerAddEnumDescriptions.cs
+++ OpenSky.API/SwaggerAddEnumDescriptions.cs
@@ -1,9 +1,14 @@
-﻿namespace OpenSky.API
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SwaggerAddEnumDescriptions.cs" company="OpenSky">
+// sushi.at for OpenSky 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc.ApiExplorer;
     using Microsoft.OpenApi.Any;
@@ -11,96 +16,68 @@
 
     using Swashbuckle.AspNetCore.SwaggerGen;
 
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Add enum value descriptions to swagger documentation.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 03/06/2021.
+    /// </remarks>
+    /// <seealso cref="T:Swashbuckle.AspNetCore.SwaggerGen.IDocumentFilter"/>
+    /// -------------------------------------------------------------------------------------------------
     public class SwaggerAddEnumDescriptions : IDocumentFilter
     {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Applies the document filter.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/06/2021.
+        /// </remarks>
+        /// <param name="swaggerDoc">
+        /// The swagger documentation.
+        /// </param>
+        /// <param name="context">
+        /// The context.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
         public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
         {
             // add enum descriptions to result models
-            foreach (var property in swaggerDoc.Components.Schemas.Where(x => x.Value?.Enum?.Count > 0))
+            foreach (var (key, value) in swaggerDoc.Components.Schemas.Where(x => x.Value?.Enum?.Count > 0))
             {
-                IList<IOpenApiAny> propertyEnums = property.Value.Enum;
-                if (propertyEnums != null && propertyEnums.Count > 0)
+                var propertyEnums = value.Enum;
+                if (propertyEnums is { Count: > 0 })
                 {
-                    property.Value.Description += DescribeEnum(propertyEnums, property.Key);
+                    value.Description += DescribeEnum(propertyEnums, key);
                 }
             }
 
             // add enum descriptions to input parameters
-            foreach (var pathItem in swaggerDoc.Paths)
+            foreach (var (key, value) in swaggerDoc.Paths)
             {
-                DescribeEnumParameters(pathItem.Value.Operations, swaggerDoc, context.ApiDescriptions, pathItem.Key);
+                this.DescribeEnumParameters(value.Operations, swaggerDoc, context.ApiDescriptions, key);
             }
         }
 
-        private void DescribeEnumParameters(IDictionary<OperationType, OpenApiOperation> operations, OpenApiDocument swaggerDoc, IEnumerable<ApiDescription> apiDescriptions, string path)
+        private static string DescribeEnum(IEnumerable<IOpenApiAny> enums, string proprtyTypeName)
         {
-            path = path.Trim('/');
-            if (operations != null)
-            {
-                var pathDescriptions = apiDescriptions.Where(a => a.RelativePath == path);
-                foreach (var oper in operations)
-                {
-                    var operationDescription = pathDescriptions.FirstOrDefault(a => a.HttpMethod.Equals(oper.Key.ToString(), StringComparison.InvariantCultureIgnoreCase));
-                    foreach (var param in oper.Value.Parameters)
-                    {
-                        var parameterDescription = operationDescription.ParameterDescriptions.FirstOrDefault(a => a.Name == param.Name);
-                        if (parameterDescription != null && TryGetEnumType(parameterDescription.Type, out Type enumType))
-                        {
-                            var paramEnum = swaggerDoc.Components.Schemas.FirstOrDefault(x => x.Key == enumType.Name);
-                            if (paramEnum.Value != null)
-                            {
-                                param.Description += DescribeEnum(paramEnum.Value.Enum, paramEnum.Key);
-                            }
-                        }
-                    }
-                }
-            }
+            var enumType = GetEnumTypeByName(proprtyTypeName);
+            if (enumType == null)
+                return null;
+
+            return " " + string.Join(", ", (from OpenApiInteger enumOption in enums select enumOption.Value into enumInt select $"{enumInt} = {Enum.GetName(enumType, enumInt)}").ToArray());
         }
 
-        bool TryGetEnumType(Type type, out Type enumType)
+        private static Type GetEnumTypeByName(string enumTypeName)
         {
-            if (type.IsEnum)
-            {
-                enumType = type;
-                return true;
-            }
-            else if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
-            {
-                var underlyingType = Nullable.GetUnderlyingType(type);
-                if (underlyingType != null && underlyingType.IsEnum == true)
-                {
-                    enumType = underlyingType;
-                    return true;
-                }
-            }
-            else
-            {
-                Type underlyingType = GetTypeIEnumerableType(type);
-                if (underlyingType != null && underlyingType.IsEnum)
-                {
-                    enumType = underlyingType;
-                    return true;
-                }
-                else
-                {
-                    var interfaces = type.GetInterfaces();
-                    foreach (var interfaceType in interfaces)
-                    {
-                        underlyingType = GetTypeIEnumerableType(interfaceType);
-                        if (underlyingType != null && underlyingType.IsEnum)
-                        {
-                            enumType = underlyingType;
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            enumType = null;
-            return false;
+            return AppDomain.CurrentDomain
+                            .GetAssemblies()
+                            .SelectMany(x => x.GetTypes())
+                            .FirstOrDefault(x => x.Name == enumTypeName);
         }
 
-        Type GetTypeIEnumerableType(Type type)
+        private static Type GetTypeIEnumerableType(Type type)
         {
             if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(IEnumerable<>))
             {
@@ -114,29 +91,109 @@
             return null;
         }
 
-        private Type GetEnumTypeByName(string enumTypeName)
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Describe enum parameters.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/06/2021.
+        /// </remarks>
+        /// <param name="operations">
+        /// The operations.
+        /// </param>
+        /// <param name="swaggerDoc">
+        /// The swagger documentation.
+        /// </param>
+        /// <param name="apiDescriptions">
+        /// The API descriptions.
+        /// </param>
+        /// <param name="path">
+        /// Full pathname of the file.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        private void DescribeEnumParameters(IDictionary<OperationType, OpenApiOperation> operations, OpenApiDocument swaggerDoc, IEnumerable<ApiDescription> apiDescriptions, string path)
         {
-            return AppDomain.CurrentDomain
-                .GetAssemblies()
-                .SelectMany(x => x.GetTypes())
-                .FirstOrDefault(x => x.Name == enumTypeName);
+            path = path.Trim('/');
+            if (operations != null)
+            {
+                var pathDescriptions = apiDescriptions.Where(a => a.RelativePath == path).ToList();
+                foreach (var (key, value) in operations)
+                {
+                    var operationDescription = pathDescriptions.FirstOrDefault(a => a.HttpMethod != null && a.HttpMethod.Equals(key.ToString(), StringComparison.InvariantCultureIgnoreCase));
+                    foreach (var param in value.Parameters)
+                    {
+                        var parameterDescription = operationDescription?.ParameterDescriptions.FirstOrDefault(a => a.Name == param.Name);
+                        if (parameterDescription != null && this.TryGetEnumType(parameterDescription.Type, out var enumType))
+                        {
+                            var paramEnum = swaggerDoc.Components.Schemas.FirstOrDefault(x => x.Key == enumType.Name);
+                            if (paramEnum.Value != null)
+                            {
+                                param.Description += DescribeEnum(paramEnum.Value.Enum, paramEnum.Key);
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-        private string DescribeEnum(IList<IOpenApiAny> enums, string proprtyTypeName)
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Try to get enum value.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/06/2021.
+        /// </remarks>
+        /// <param name="type">
+        /// The type.
+        /// </param>
+        /// <param name="enumType">
+        /// [out] Type of the enum.
+        /// </param>
+        /// <returns>
+        /// True if it succeeds, false if it fails.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        private bool TryGetEnumType(Type type, out Type enumType)
         {
-            List<string> enumDescriptions = new List<string>();
-            var enumType = GetEnumTypeByName(proprtyTypeName);
-            if (enumType == null)
-                return null;
-
-            foreach (OpenApiInteger enumOption in enums)
+            if (type.IsEnum)
             {
-                int enumInt = enumOption.Value;
-
-                enumDescriptions.Add(string.Format("{0} = {1}", enumInt, Enum.GetName(enumType, enumInt)));
+                enumType = type;
+                return true;
+            }
+            else if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                var underlyingType = Nullable.GetUnderlyingType(type);
+                if (underlyingType is { IsEnum: true })
+                {
+                    enumType = underlyingType;
+                    return true;
+                }
+            }
+            else
+            {
+                var underlyingType = GetTypeIEnumerableType(type);
+                if (underlyingType is { IsEnum: true })
+                {
+                    enumType = underlyingType;
+                    return true;
+                }
+                else
+                {
+                    var interfaces = type.GetInterfaces();
+                    foreach (var interfaceType in interfaces)
+                    {
+                        underlyingType = GetTypeIEnumerableType(interfaceType);
+                        if (underlyingType is { IsEnum: true })
+                        {
+                            enumType = underlyingType;
+                            return true;
+                        }
+                    }
+                }
             }
 
-            return string.Join(", ", enumDescriptions.ToArray());
+            enumType = null;
+            return false;
         }
     }
 }
