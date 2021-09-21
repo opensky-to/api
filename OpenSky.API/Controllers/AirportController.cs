dd16c9c2e526f365968383a0171a4a5f5ed7bb2c
sushi.at <sushi@opensky.to>
2021-09-21 15:58:26 +0100

    Added package and package hash methods

--- OpenSky.API/Controllers/AirportController.cs
+++ OpenSky.API/Controllers/AirportController.cs
@@ -78,67 +78,6 @@ namespace OpenSky.API.Controllers
 
         /// -------------------------------------------------------------------------------------------------
         /// <summary>
-        /// Gets one specific airport.
-        /// </summary>
-        /// <remarks>
-        /// sushi.at, 03/05/2021.
-        /// </remarks>
-        /// <param name="icao">
-        /// The ICAO identifier of the airport.
-        /// </param>
-        /// <returns>
-        /// The airport record if found, nothing otherwise.
-        /// </returns>
-        /// -------------------------------------------------------------------------------------------------
-        [HttpGet("{icao}", Name = "GetAirport")]
-        public async Task<ActionResult<ApiResponse<Airport>>> GetAirport(string icao)
-        {
-            try
-            {
-                this.logger.LogInformation($"{this.User.Identity?.Name} | GET Airport/{icao}");
-                var airport = await this.db.Airports.SingleOrDefaultAsync(a => a.ICAO.Equals(icao));
-                // todo add country(ies) by looking up ICAO registration
-                return new ApiResponse<Airport>(airport);
-            }
-            catch (Exception ex)
-            {
-                this.logger.LogError(ex, $"{this.User.Identity?.Name} | GET Airport/{icao}");
-                return new ApiResponse<Airport>(ex);
-            }
-        }
-
-        /// -------------------------------------------------------------------------------------------------
-        /// <summary>
-        /// Searches for airports using ICAO, name or city and return the first 50 results.
-        /// </summary>
-        /// <remarks>
-        /// sushi.at, 21/09/2021.
-        /// </remarks>
-        /// <param name="searchString">
-        /// The search string.
-        /// </param>
-        /// <returns>
-        /// The matching airport records.
-        /// </returns>
-        /// -------------------------------------------------------------------------------------------------
-        [HttpGet("search/{searchString}", Name = "SearchAirport")]
-        public async Task<ActionResult<ApiResponse<IEnumerable<Airport>>>> SearchAirport(string searchString)
-        {
-            try
-            {
-                this.logger.LogInformation($"{this.User.Identity?.Name} | GET Airport/search/{searchString}");
-                var airports = await this.db.Airports.Where(a => a.ICAO.Contains(searchString) || a.City.Contains(searchString) || a.Name.Contains(searchString)).Take(50).ToListAsync();
-                return new ApiResponse<IEnumerable<Airport>>(airports);
-            }
-            catch (Exception ex)
-            {
-                this.logger.LogError(ex, $"{this.User.Identity?.Name} | GET Airport/search/{searchString}");
-                return new ApiResponse<IEnumerable<Airport>>(ex);
-            }
-        }
-
-        /// -------------------------------------------------------------------------------------------------
-        /// <summary>
         /// Creates a new airport client package.
         /// </summary>
         /// <remarks>
@@ -200,6 +139,102 @@ namespace OpenSky.API.Controllers
 
         /// -------------------------------------------------------------------------------------------------
         /// <summary>
+        /// Gets one specific airport.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/05/2021.
+        /// </remarks>
+        /// <param name="icao">
+        /// The ICAO identifier of the airport.
+        /// </param>
+        /// <returns>
+        /// The airport record if found, nothing otherwise.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        [HttpGet("{icao}", Name = "GetAirport")]
+        public async Task<ActionResult<ApiResponse<Airport>>> GetAirport(string icao)
+        {
+            try
+            {
+                this.logger.LogInformation($"{this.User.Identity?.Name} | GET Airport/{icao}");
+                var airport = await this.db.Airports.SingleOrDefaultAsync(a => a.ICAO.Equals(icao));
+
+                // todo add country(ies) by looking up ICAO registration
+                return new ApiResponse<Airport>(airport);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, $"{this.User.Identity?.Name} | GET Airport/{icao}");
+                return new ApiResponse<Airport>(ex);
+            }
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the latest airport client package.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 21/09/2021.
+        /// </remarks>
+        /// <returns>
+        /// The airport client package.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        [HttpGet("clientPackage", Name = "GetAirportClientPackage")]
+        public async Task<ActionResult<ApiResponse<AirportClientPackage>>> GetAirportClientPackage()
+        {
+            try
+            {
+                this.logger.LogInformation($"{this.User.Identity?.Name} | GET Airport/clientPackage");
+                var latestClientPackage = await this.db.AirportClientPackages.OrderByDescending(p => p.CreationTime).FirstOrDefaultAsync();
+                if (latestClientPackage == null)
+                {
+                    return new ApiResponse<AirportClientPackage>("No airport client package available.") { IsError = true };
+                }
+
+                return new ApiResponse<AirportClientPackage>(latestClientPackage);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, $"{this.User.Identity?.Name} | GET Airport/clientPackage");
+                return new ApiResponse<AirportClientPackage>(ex);
+            }
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Gets the latest airport client package hash.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 21/09/2021.
+        /// </remarks>
+        /// <returns>
+        /// The airport package hash.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        [HttpGet("clientPackageHash", Name = "GetAirportClientPackageHash")]
+        public async Task<ActionResult<ApiResponse<string>>> GetAirportClientPackageHash()
+        {
+            try
+            {
+                this.logger.LogInformation($"{this.User.Identity?.Name} | GET Airport/clientPackageHash");
+                var latestClientPackage = await this.db.AirportClientPackages.OrderByDescending(p => p.CreationTime).FirstOrDefaultAsync();
+                if (latestClientPackage == null)
+                {
+                    return new ApiResponse<string>("No airport client package available.") { IsError = true };
+                }
+
+                return new ApiResponse<string> { Data = latestClientPackage.PackageHash };
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, $"{this.User.Identity?.Name} | GET Airport/clientPackageHash");
+                return new ApiResponse<string>(ex);
+            }
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
         /// Gets all available airports (ICAO code only).
         /// </summary>
         /// <remarks>
@@ -249,12 +284,43 @@ namespace OpenSky.API.Controllers
             {
                 this.logger.LogInformation($"{this.User.Identity?.Name} | GET AirportsWithPopulationStatus/{status}/{maxResults}");
                 var airport = await this.db.Airports.Where(a => a.HasBeenPopulated == status).Take(maxResults).ToListAsync();
+
                 // todo add country(ies) by looking up ICAO registration
                 return new ApiResponse<IEnumerable<Airport>>(airport);
             }
             catch (Exception ex)
             {
                 this.logger.LogError(ex, $"{this.User.Identity?.Name} | GET AirportsWithPopulationStatus/{status}/{maxResults}");
+                return new ApiResponse<IEnumerable<Airport>>(ex);
+            }
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Searches for airports using ICAO, name or city and return the first 50 results.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 21/09/2021.
+        /// </remarks>
+        /// <param name="searchString">
+        /// The search string.
+        /// </param>
+        /// <returns>
+        /// The matching airport records.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        [HttpGet("search/{searchString}", Name = "SearchAirport")]
+        public async Task<ActionResult<ApiResponse<IEnumerable<Airport>>>> SearchAirport(string searchString)
+        {
+            try
+            {
+                this.logger.LogInformation($"{this.User.Identity?.Name} | GET Airport/search/{searchString}");
+                var airports = await this.db.Airports.Where(a => a.ICAO.Contains(searchString) || a.City.Contains(searchString) || a.Name.Contains(searchString)).Take(50).ToListAsync();
+                return new ApiResponse<IEnumerable<Airport>>(airports);
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, $"{this.User.Identity?.Name} | GET Airport/search/{searchString}");
                 return new ApiResponse<IEnumerable<Airport>>(ex);
             }
         }
