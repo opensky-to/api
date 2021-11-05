cbd26c229986dbf80539fbe5266d396d2189a152
sushi.at <sushi@opensky.to>
2021-11-05 13:36:52 +0000

    Fixed navlog fixes table name

---
+++ OpenSky.API/Migrations/20211105110208_FlightPlanRouting.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenSky.API.Migrations
+{
+    public partial class FlightPlanRouting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AlternateRoute",
+                table: "Flights",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OfpHtml",
+                table: "Flights",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Route",
+                table: "Flights",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "FlightNavlogFixes",
+                columns: table => new
+                {
+                    FixNumber = table.Column<int>(type: "int", nullable: false),
+                    FlightID = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Ident = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Latitude = table.Column<double>(type: "double", nullable: false),
+                    Longitude = table.Column<double>(type: "double", nullable: false),
+                    Type = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightNavlogFixes", x => new { x.FlightID, x.FixNumber });
+                    table.ForeignKey(
+                        name: "FK_FlightNavlogFixes_Flights_FlightID",
+                        column: x => x.FlightID,
+                        principalTable: "Flights",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FlightNavlogFixes");
+
+            migrationBuilder.DropColumn(
+                name: "AlternateRoute",
+                table: "Flights");
+
+            migrationBuilder.DropColumn(
+                name: "OfpHtml",
+                table: "Flights");
+
+            migrationBuilder.DropColumn(
+                name: "Route",
+                table: "Flights");
+        }
+    }
+}
