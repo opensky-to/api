387e2377b6ebf2751adbee26376316be64a71be3
sushi.at <sushi@opensky.to>
2021-11-10 17:27:19 +0000

    Added fuel weight and start flight method

---
+++ OpenSky.API/DbModel/Enums/FuelType.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FuelType.cs" company="OpenSky">
+// OpenSky project 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API.DbModel.Enums
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Fuel types (derived from engine type, SimConnect doesn't report this).
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 10/11/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public enum FuelType
+    {
+        /// <summary>
+        /// Aviation Gas, only used by piston engines.
+        /// </summary>
+        AvGas = 0,
+
+        /// <summary>
+        /// Jet fuel.
+        /// </summary>
+        JetFuel = 1,
+
+        /// <summary>
+        /// No fuel.
+        /// </summary>
+        None = 2
+    }
+}
