527943228ec5836f3922b87f330940614bcb2d20
sushi.at <sushi@opensky.to>
2021-11-15 23:49:48 +0000

    Added flight log method for client logbook
    Added landed at to flight

---
+++ OpenSky.API/Migrations/20211115225800_LandedAt.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpenSky.API.Migrations
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <content>
+    /// Flight landed at migration.
+    /// </content>
+    /// -------------------------------------------------------------------------------------------------
+    public partial class LandedAt : Migration
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// <para>
+        ///                 Builds the operations that will migrate the database 'up'.
+        ///             </para>
+        /// <para>
+        ///                 That is, builds the operations that will take the database from the state
+        ///                 left in by the previous migration so that it is up-to-date with regard to
+        ///                 this migration.
+        ///             </para>
+        /// <para>
+        ///                 This method must be overridden in each class the inherits from
+        ///                 <see cref="T:Microsoft.EntityFrameworkCore.Migrations.Migration" />.
+        ///             </para>
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 15/11/2021.
+        /// </remarks>
+        /// <param name="migrationBuilder">
+        /// The <see cref="T:Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder" /> that will
+        /// build the operations.
+        /// </param>
+        /// <seealso cref="M:Microsoft.EntityFrameworkCore.Migrations.Migration.Up(MigrationBuilder)"/>
+        /// -------------------------------------------------------------------------------------------------
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LandedAtICAO",
+                table: "Flights",
+                type: "varchar(5)",
+                maxLength: 5,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_LandedAtICAO",
+                table: "Flights",
+                column: "LandedAtICAO");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Flights_Airports_LandedAtICAO",
+                table: "Flights",
+                column: "LandedAtICAO",
+                principalTable: "Airports",
+                principalColumn: "ICAO",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// <para>
+        ///                 Builds the operations that will migrate the database 'down'.
+        ///             </para>
+        /// <para>
+        ///                 That is, builds the operations that will take the database from the state
+        ///                 left in by this migration so that it returns to the state that it was in
+        ///                 before this migration was applied.
+        ///             </para>
+        /// <para>
+        ///                 This method must be overridden in each class the inherits from
+        ///                 <see cref="T:Microsoft.EntityFrameworkCore.Migrations.Migration" /> if both
+        ///                 'up' and 'down' migrations are to be supported. If it is not overridden, then
+        ///                 calling it will throw and it will not be possible to migrate in the 'down'
+        ///                 direction.
+        ///             </para>
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 15/11/2021.
+        /// </remarks>
+        /// <param name="migrationBuilder">
+        /// The <see cref="T:Microsoft.EntityFrameworkCore.Migrations.MigrationBuilder" /> that will
+        /// build the operations.
+        /// </param>
+        /// <seealso cref="M:Microsoft.EntityFrameworkCore.Migrations.Migration.Down(MigrationBuilder)"/>
+        /// -------------------------------------------------------------------------------------------------
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Flights_Airports_LandedAtICAO",
+                table: "Flights");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Flights_LandedAtICAO",
+                table: "Flights");
+
+            migrationBuilder.DropColumn(
+                name: "LandedAtICAO",
+                table: "Flights");
+        }
+    }
+}
