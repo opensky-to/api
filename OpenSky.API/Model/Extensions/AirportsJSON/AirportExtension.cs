222c74558709e41f1480f0c517a16ec655ff783f
sushi.at <sushi@opensky.to>
2021-12-03 18:13:49 +0000

    Switched to AirportJSON project, removed airport client package classes

---
+++ OpenSky.API/Model/Extensions/AirportsJSON/AirportExtension.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Airport.cs" company="OpenSky">
+// OpenSky project 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API.Model.Extensions.AirportsJSON
+{
+    using OpenSky.API.DbModel;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Airport extension to quickly create JSON package airport.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 03/12/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public static class AirportExtension
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// An Airport extension method that constructs an airports JSON airport.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/12/2021.
+        /// </remarks>
+        /// <param name="airport">
+        /// The airport to act on.
+        /// </param>
+        /// <returns>
+        /// An OpenSky.AirportsJSON.Airport.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        public static OpenSky.AirportsJSON.Airport ConstructAirportsJson(this Airport airport)
+        {
+            return new OpenSky.AirportsJSON.Airport
+            {
+                ICAO = airport.ICAO,
+                Name = airport.Name,
+                Latitude = airport.Latitude,
+                Longitude = airport.Longitude,
+                Size = airport.Size ?? -1,
+                HasAvGas = airport.HasAvGas,
+                HasJetFuel = airport.HasJetFuel,
+                IsClosed = airport.IsClosed,
+                IsMilitary = airport.IsMilitary,
+                SupportsSuper = airport.SupportsSuper,
+                MSFS = airport.MSFS
+            };
+        }
+    }
+}
---
+++ OpenSky.API/Model/Extensions/AirportsJSON/RunwayExtension.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RunwayExtension.cs" company="OpenSky">
+// OpenSky project 2021
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API.Model.Extensions.AirportsJSON
+{
+    using OpenSky.API.DbModel;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Runway extension to quickly create JSON package runway.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 03/12/2021.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public static class RunwayExtension
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// A Runway extension method that constructs an airports JSON runway.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 03/12/2021.
+        /// </remarks>
+        /// <param name="runway">
+        /// The runway to act on.
+        /// </param>
+        /// <returns>
+        /// An OpenSky.AirportsJSON.Runway.
+        /// </returns>
+        /// -------------------------------------------------------------------------------------------------
+        public static OpenSky.AirportsJSON.Runway ConstructAirportsJson(this Runway runway)
+        {
+            return new OpenSky.AirportsJSON.Runway
+            {
+                Length = runway.Length,
+                HasLighting = !string.IsNullOrEmpty(runway.CenterLight) || !string.IsNullOrEmpty(runway.EdgeLight)
+            };
+        }
+    }
+}
