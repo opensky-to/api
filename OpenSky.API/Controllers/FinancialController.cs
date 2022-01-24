bce1f0cb5b7069c3476e3ca05bce80f814e8c3ce
sushi.at <sushi@opensky.to>
2022-01-24 12:41:15 +0000

    Added financial records model and migration
    Added skeleton financial controller

---
+++ OpenSky.API/Controllers/FinancialController.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FinancialController.cs" company="OpenSky">
+// OpenSky project 2022
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace OpenSky.API.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    using OpenSky.API.DbModel;
+    using OpenSky.API.Model.Authentication;
+
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Financial controller.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 24/01/2022.
+    /// </remarks>
+    /// <seealso cref="T:Microsoft.AspNetCore.Mvc.ControllerBase"/>
+    /// -------------------------------------------------------------------------------------------------
+    [Authorize(Roles = UserRoles.User)]
+    [ApiController]
+    [Microsoft.AspNetCore.Components.Route("[controller]")]
+    public class FinancialController : ControllerBase
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// The OpenSky database context.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        private readonly OpenSkyDbContext db;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        private readonly ILogger<FinancialController> logger;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// The user manager.
+        /// </summary>
+        /// -------------------------------------------------------------------------------------------------
+        private readonly UserManager<OpenSkyUser> userManager;
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FinancialController"/> class.
+        /// </summary>
+        /// <remarks>
+        /// sushi.at, 24/01/2022.
+        /// </remarks>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        /// <param name="db">
+        /// The OpenSky database context.
+        /// </param>
+        /// <param name="userManager">
+        /// The user manager.
+        /// </param>
+        /// -------------------------------------------------------------------------------------------------
+        public FinancialController(ILogger<FinancialController> logger, OpenSkyDbContext db, UserManager<OpenSkyUser> userManager)
+        {
+            this.logger = logger;
+            this.db = db;
+            this.userManager = userManager;
+        }
+    }
+}
