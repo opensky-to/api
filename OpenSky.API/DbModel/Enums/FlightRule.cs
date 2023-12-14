06e986a1d1bb627ae4470778b5a4a002da43b583
sushi.at <sushi@opensky.to>
2023-12-14 22:13:06 +0000

    Added flight rule to flights and plans

---
+++ OpenSky.API/DbModel/Enums/FlightRule.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FlightRule.cs" company="OpenSky">
+// OpenSky project 2021-2023
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable InconsistentNaming
+
+namespace OpenSky.API.DbModel.Enums
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    /// Flight rules.
+    /// </summary>
+    /// <remarks>
+    /// sushi.at, 14/12/2023.
+    /// </remarks>
+    /// -------------------------------------------------------------------------------------------------
+    public enum FlightRule
+    {
+        /// <summary>
+        /// Instrument
+        /// </summary>
+        IFR = 0,
+
+        /// <summary>
+        /// Visual
+        /// </summary>
+        VFR = 1,
+
+        /// <summary>
+        /// Instrument -> Visual
+        /// </summary>
+        IFRtoVFR = 2,
+
+        /// <summary>
+        /// Visual -> Instrument
+        /// </summary>
+        VFRtoIFR = 3
+    }
+}
