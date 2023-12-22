d755d47f8ebb33a28c1631edd6776a634ec0f136
sushi.at <sushi@opensky.to>
2023-12-22 21:17:14 +0000

    Removed bing maps key

---
+++ OpenSky.API/Migrations/20231222130054_RemoveBingMapsKey.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenSky.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBingMapsKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BingMapsKey",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BingMapsKey",
+                table: "Users",
+                type: "varchar(200)",
+                maxLength: 200,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
