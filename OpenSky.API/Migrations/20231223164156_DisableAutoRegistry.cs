bb22a13bbfac8bc1a1eded794b5ccaa84cca786e
sushi.at <sushi@opensky.to>
2023-12-23 16:43:51 +0000

    DB only settable "disable auto registry" added to types

---
+++ OpenSky.API/Migrations/20231223164156_DisableAutoRegistry.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenSky.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DisableAutoRegistry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "DisableAutoRegistry",
+                table: "AircraftTypes",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisableAutoRegistry",
+                table: "AircraftTypes");
+        }
+    }
+}
